5af2d3ede0740e372e67ef87d191ddc919367e77
Heiko Hübscher <heiko.huebscher@idealo.de>
2019-03-20 13:49:52 +0100

    [jquery] add `jquery-perf` w/ extends jquery

---
+++ scripts/jquery-perf-1.8.3.js
@@ -0,0 +1,161 @@
+define(['jquery'], function(jQuery) {
+    var core_hasOwn = Object.prototype.hasOwnProperty;
+    if (typeof Object.prototype.hasOwnProperty !== 'function') {
+        Object.prototype.hasOwnProperty = function (name) {
+            var proto = this.__proto__ || this.constructor.prototype;
+            return name in this && ( !(name in proto) || this[name] !== proto[name] );
+        };
+    }
+
+
+    jQuery.extend({
+        isPlainObject: function (obj) {
+            // Must be an Object.
+            // Because of IE, we also have to check the presence of the constructor property.
+            // Make sure that DOM nodes and window objects don't pass through, as well
+            if (!obj || jQuery.type(obj) !== "object" || obj.nodeType || jQuery.isWindow(obj)) {
+                return false;
+            }
+
+            //try {
+            //    console.log('try');
+            // Not own constructor property must be Object
+            if (obj.constructor && !core_hasOwn.call(obj, "constructor") && !core_hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+                return false;
+            }
+            //} catch ( e ) {
+            ////    // IE8,9 Will throw exceptions on certain host objects #9897
+            //    return false;
+            //}
+
+            // Own properties are enumerated firstly, so to speed up,
+            // if last one is own, then all properties are own.
+
+            var key;
+            for (key in obj) {
+            }
+
+            return key === undefined || core_hasOwn.call(obj, key);
+        }
+    });
+    // Give the init function the jQuery prototype for later instantiation
+    jQuery.fn.init.prototype = jQuery.fn;
+
+    jQuery.extend = jQuery.fn.extend = function () {
+        var options, name, src, copy, copyIsArray, clone,
+            target = arguments[0] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if (typeof target === "boolean") {
+            deep = target;
+            target = arguments[1] || {};
+            // skip the boolean and the target
+            i = 2;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy) -> ist es nicht, weil das target einfach immer durchgereicht wird und wenns kein obj initial ist oder func, dann kann man es eh nicht extenden und ne js error ist berechtigt
+        //if(deep === true) {
+        //if (typeof target !== "object" && !jQuery.isFunction(target) ) {
+        //    target = {};
+        //}
+        //}
+
+        // extend jQuery itself if only one argument is passed
+        if (length === i) {
+            target = this;
+            --i;
+        }
+
+        for (; i < length; i++) {
+            // Only deal with non-null/undefined values
+            if ((options = arguments[i]) != null) {
+                // Extend the base object
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+
+                    // Prevent never-ending loop
+                    if (target === copy) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if (deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) )) {
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && jQuery.isArray(src) ? src : [];
+
+                        } else {
+                            clone = src && jQuery.isPlainObject(src) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[name] = jQuery.extendDeep(clone, copy);
+
+                        // Don't bring in undefined values
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
+    };
+
+    jQuery.extendDeep = jQuery.fn.extendDeep = function () {
+        var options, name, src, copy, copyIsArray, clone,
+            target = arguments[0] || {},
+            i = 1,
+            length = arguments.length;
+
+
+        // extend jQuery itself if only one argument is passed
+        if (length === i) {
+            target = this;
+            --i;
+        }
+
+        for (; i < length; i++) {
+            // Only deal with non-null/undefined values
+            if ((options = arguments[i]) != null) {
+                // Extend the base object
+                for (name in options) {
+
+                    src = target[name];
+                    copy = options[name];
+
+                    // Prevent never-ending loop
+                    if (target === copy) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if (copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) )) {
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && jQuery.isArray(src) ? src : [];
+
+                        } else {
+                            clone = src && jQuery.isPlainObject(src) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[name] = jQuery.extendDeep(clone, copy);
+
+                        // Don't bring in undefined values
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
+    };
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 requirejs([
     'scripts/helper/util',
     'jquery',
+    'jquery-perf',
 ], function(util, $) {
     console.log('main loading')
     util()
     console.log('global jquery version', jQuery.fn.jquery)
     console.log(util, $)
+    console.log('extendDeep is', $.extendDeep)
 })
