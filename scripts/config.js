f634fcccbb2db618767d498367c75c788f2b68ba
Heiko Hübscher <heiko.huebscher@idealo.de>
2019-03-21 10:51:33 +0100

    [requirejs] fix build
    move mapping config from requirejs config into source code. thus it gets
    included in compilat.

--- scripts/config.js
+++ scripts/config.js
@@ -4,15 +4,4 @@ require.config({
         'jquery-private': 'scripts/jquery-private',
         'jquery-perf': 'scripts/jquery-perf-1.8.3',
     },
-    map: {
-        '*': {
-            'jquery': 'jquery-private',
-        },
-        'jquery-private': {
-            'jquery': 'jquery',
-        },
-        'jquery-perf': {
-            'jquery': 'jquery',
-        },
-    }
 })
--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,15 @@
+require.config({
+    map: {
+        '*': {
+            'jquery': 'jquery-private',
+        },
+        'jquery-private': {
+            'jquery': 'jquery',
+        },
+        'jquery-perf': {
+            'jquery': 'jquery',
+        },
+    }
+})
+
 require(['scripts/main'])
